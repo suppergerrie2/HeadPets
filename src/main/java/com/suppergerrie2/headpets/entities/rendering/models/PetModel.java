ba0c33962f06507eb777272c6307365c9e6591de
suppergerrie2 <gerjan2001@gmail.com>
2018-06-16 13:05:07 +0200

    Simple animation when sitting

--- src/main/java/com/suppergerrie2/headpets/entities/rendering/models/PetModel.java
+++ src/main/java/com/suppergerrie2/headpets/entities/rendering/models/PetModel.java
@@ -1,6 +1,7 @@
 package com.suppergerrie2.headpets.entities.rendering.models;
 
 import com.suppergerrie2.headpets.entities.EntityHead;
+import com.suppergerrie2.headpets.entities.EntityHeadPet;
 
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelDragonHead;
@@ -35,6 +36,19 @@ public class PetModel extends ModelBase {
 			modelbase = this.dragonHead;
 		default:
 			break;
+		}
+		
+		if(entity instanceof EntityHeadPet) {
+			if(((EntityHeadPet) entity).isSitting()) {
+				float time = 20;
+				float offset = ageInTicks%time;
+				offset/=time;
+				offset*=2*Math.PI;
+				offset = (float) (Math.sin(offset)+1);
+				offset*=0.015;
+				GlStateManager.scale(1+offset, 1+offset, 1+offset);
+				GlStateManager.translate(0, -offset*2, 0);
+			}
 		}
 
 		GlStateManager.translate(0, 1.5F, 0);
